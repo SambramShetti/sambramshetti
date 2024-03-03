f080a281fe26af3e368d2b2ae6c7f2c1142e8e70
Sambram <sambram016@gmail.com>
2024-03-03 13:41:51 +0530

    django pagination and search bar codes

---
+++ Django_Pagination/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+
+def home(request):
+    product = Product.objects.all()
+
+    # Search bar codes
+    item_name = request.GET.get('item_name')
+    if item_name != "" and item_name != None:
+        product = product.filter(title__icontains = item_name)
+
+    # pagination codes
+    paginator = Paginator(product, 2)
+    page = request.GET.get('page')
+    product = paginator.get_page(page)
+
+    return render(request, 'myapp/home.html',{'product':product})
